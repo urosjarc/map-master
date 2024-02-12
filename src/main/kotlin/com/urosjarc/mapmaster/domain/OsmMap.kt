ddb4ce80f158457d28a054d3e63b8af381efdbc9
urosjarc <jar.fmf@gmail.com>
2024-02-12 11:50:01 +0100

    Finished with first version of A* route searching algorithm.

--- src/main/kotlin/com/urosjarc/mapmaster/domain/OsmMap.kt
+++ src/main/kotlin/com/urosjarc/mapmaster/domain/OsmMap.kt
@@ -1,9 +1,11 @@
 package com.urosjarc.mapmaster.domain
 
 import com.urosjarc.mapmaster.Utils
+import com.urosjarc.mapmaster.exceptions.OsmException
 import com.urosjarc.mapmaster.rules.CyclewayTypeSuitability
 import com.urosjarc.mapmaster.rules.FootwayTypeSuitability
 import com.urosjarc.mapmaster.rules.HighwayTypeSuitability
+import java.util.*
 
 data class OsmMap(
     val minLat: Double,
@@ -13,9 +15,8 @@ data class OsmMap(
 ) {
 
     val features = OsmFeatures()
-    private val streets_to_features = mutableMapOf<String, MutableList<OsmFeature>>()
-    private val positions_to_streets = mutableMapOf<MapPosition, OsmNode>()
-
+    private val street_to_feature = mutableMapOf<String, MutableList<OsmFeature>>()
+    private val position_to_street = mutableMapOf<MapPosition, OsmNode>()
 
     fun add(feature: OsmFeature): Boolean {
         val street = feature.obj.street
@@ -23,7 +24,7 @@ data class OsmMap(
 
         // Add street mapping
         if (street != null) {
-            this.streets_to_features
+            this.street_to_feature
                 .getOrPut(feature.obj.address!!, ::mutableListOf)
                 .add(feature)
         }
@@ -32,7 +33,7 @@ data class OsmMap(
         if (feature.objType == OsmFeature.Type.NODE) {
             val node = feature.obj as OsmNode
             if (street != null && houseNumber != null) {
-                this.positions_to_streets[node.position] = node
+                this.position_to_street[node.position] = node
             }
         }
 
@@ -64,18 +65,18 @@ data class OsmMap(
 
 
     fun getStreetFeatures(name: String): MutableList<OsmFeature>? {
-        return this.streets_to_features[name]
+        return this.street_to_feature[name]
     }
 
     /**
      * SEARCHING
      */
     fun searchStreet(query: String, limit: Int = 10): List<MapMatch> =
-        Utils.search(query = query, choices = this.streets_to_features.keys, limit = limit)
+        Utils.search(query = query, choices = this.street_to_feature.keys, limit = limit)
 
     fun searchClosestStreets(position: MapPosition, radius: Float = 100f): List<MapMatch> {
         var matches = mutableListOf<MapMatch>()
-        this.positions_to_streets.forEach { (pos, node) ->
+        this.position_to_street.forEach { (pos, node) ->
             val distance = position.distance(position = pos)
             val address = node.address
             if (distance < radius && address != null) {
@@ -88,7 +89,83 @@ data class OsmMap(
         return matches
     }
 
-    fun searchShortestPath(start: MapPosition, end: MapPosition) {
+    fun costEstimation(start: OsmNode, end: OsmNode, vehicle: OsmVehicle): Double {
+        //TODO: Calculate cartesian distance with elevation data
+        //TODO: Calculate node density, type, etc.. to approximate how long it would take to travel the air distance to finish
+        return start.position.distance(end.position)
+    }
 
+    fun searchShortestTransitWay(
+        start: MapPosition,
+        end: MapPosition,
+        vehicle: OsmVehicle,
+        suitability: OsmSuitability
+    ): MutableList<OsmNode> {
+
+        //Get search space
+        val ways = this.getTransitWays(vehicle = vehicle, suitability = suitability).toMutableList()
+
+        //Get starting and end node
+        var startNode: OsmNode = ways[0].nodes[0]
+        var endNode: OsmNode = startNode
+
+        var minStartDist = Double.MAX_VALUE
+        var minEndDist = Double.MAX_VALUE
+
+        //Search travel space for closest nodes
+        var numNodes = 0
+        ways.forEach { way ->
+            way.nodes.forEach { node ->
+                numNodes++
+                val toStartDist = start.distance(position = node.position)
+                val toEndDist = end.distance(position = node.position)
+                if (toStartDist < minStartDist) {
+                    startNode = node
+                    minStartDist = toStartDist
+                }
+                if (toEndDist < minEndDist) {
+                    endNode = node
+                    minEndDist = toEndDist
+                }
+            }
+        }
+
+        //Cost of cheepest path
+        val gScore = mutableMapOf(startNode to 0.0)
+        val fScore = mutableMapOf(startNode to this.costEstimation(start = startNode, end = endNode, vehicle = vehicle))
+
+        //Weighted A* algorithm
+        val discoveredNodes = PriorityQueue<OsmNode>(numNodes, compareBy { fScore[it]!! })
+        val cameFrom = mutableMapOf<OsmNode, OsmNode>()
+
+
+        //Traverse search space
+        while (ways.isNotEmpty()) {
+            var current = discoveredNodes.remove()
+            if (current == endNode) {
+                val route = mutableListOf(current)
+                while (cameFrom.contains(key = current)) {
+                    current = cameFrom[current]
+                    route.add(0, current)
+                }
+                return route
+            }
+
+            current.siblings.forEach { sibling ->
+                val siblingsGScore = gScore[current]!!
+                val tentativeScore = siblingsGScore + vehicle.cost(start = current, end = sibling)
+                if (tentativeScore < siblingsGScore) {
+                    cameFrom[sibling] = current
+                    gScore[sibling] = tentativeScore
+                    fScore[sibling] = tentativeScore + this.costEstimation(start = startNode, end = endNode, vehicle = vehicle))
+                    if (!discoveredNodes.contains(sibling)) {
+                        discoveredNodes.add(sibling)
+                    }
+                }
+            }
+
+        }
+
+        throw OsmException("End node was not found in search space")
     }
 }
--- src/main/kotlin/com/urosjarc/mapmaster/domain/OsmVehicle.kt
+++ src/main/kotlin/com/urosjarc/mapmaster/domain/OsmVehicle.kt
@@ -6,5 +6,11 @@ data class OsmVehicle(
     val height: Float,
     val type: Type
 ) {
+    fun cost(start: OsmNode, end: OsmNode): Double {
+        //Calculate accurate money cost to go from the start to the end node
+        //Use elevated cartesian distance, road type etc...
+        return start.position.distance(end.position)
+    }
+
     enum class Type { BODY, HORSE, BICYCLE, MOPED, MOTORCYCLE, CAR, BUS, TRACTOR, TRAIN }
 }
